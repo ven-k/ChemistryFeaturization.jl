5ed80466974a604f63a142f76d0bf4ea48f4e832
Venkateshprasad <vensadk@gmail.com>
2021-12-16 22:14:32 +0530

    Try uninstalling the MKL after checking if it actually exists

--- deps/build.jl
+++ deps/build.jl
@@ -3,5 +3,11 @@ using Conda
 Conda.add("ase", channel = "conda-forge")
 Conda.add("rdkit", channel = "conda-forge")
 Conda.add("pymatgen", channel = "conda-forge")
-Conda.rm("mkl")
-Conda.add("mkl")
+try
+  Conda.version("mkl")
+  Conda.rm("mkl")
+catch e
+  @info "\nPre-existing MKL is uninstalled to avoid known issues.\nNow reinstalling the MKL..."
+finally
+  Conda.add("mkl")
+end
